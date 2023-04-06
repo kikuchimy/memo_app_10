20797875a2eeb26c3142f58e619826165e073c4c
“kikuchimy” <m.kikuchi@next-revolution.net>
2023-04-06 16:15:00 +0900

    データの更新、削除

--- resources/views/memos/show.blade.php
+++ resources/views/memos/show.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>memo show</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     {{-- <h1>タイトル</h1> --}}
@@ -13,5 +14,17 @@
     <a href="/memos">戻る</a>
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(e($memo->body)) !!}</p>
+
+    {{-- <!-- $memoのidを元に編集ページへ遷移する --> --}}
+    {{-- <button onclick="location.href='/memos/{{ $memo->id }}/edit'">編集する</button> --}}
+    <div class="button-group">
+        <!-- $memoのidを元に編集ページへ遷移する -->
+        <button onclick="location.href='/memos/{{ $memo->id }}/edit'">編集する</button>
+        <form action="/memos/{{ $memo->id }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+        </form>
+    </div>
 </body>
 </html>
